1d792e137a924ce7d5d977804803dec5d9069fe8
Sebastian Loaiza <slmartinez19@gmail.com>
2019-11-19 23:36:49 -0500

    ajustes

--- src/app/pages/pedidos/pedidos.component.ts
+++ src/app/pages/pedidos/pedidos.component.ts
@@ -1,39 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { ListarPerdidosService } from 'src/app/services/service.index';
-
+import { Component, OnInit } from "@angular/core";
+import { ListarPerdidosService } from "src/app/services/service.index";
 
 @Component({
-  selector: 'app-pedidos',
-  templateUrl: './pedidos.component.html',
-  styleUrls: ['./pedidos.component.scss']
+  selector: "app-pedidos",
+  templateUrl: "./pedidos.component.html",
+  styleUrls: ["./pedidos.component.scss"]
 })
 export class PedidosComponent implements OnInit {
-
   objects: any = [];
-
-  constructor(private _listarPerdidosService: ListarPerdidosService) { }
-
+  constructor(private _listarPerdidosService: ListarPerdidosService) {}
   ngOnInit() {
-    this._listarPerdidosService.getPedidos().subscribe((res:any) => {
+    this._listarPerdidosService.getPedidos().subscribe((res: any) => {
       for (var i = 0; i < res.orders.length; i++) {
         var data = {};
-        data = 
-          {
-            pedido: res.orders[i].id,
-            fecha: res.orders[i].created,
-            nombre: res.orders[i].user.full_name,
-            estado: res.orders[i].status.name,
-            observaciones: res.orders[i].order_products[0].product.description,
-            direccion: (res.orders[i].location ? res.orders[i].location.address : ""),
-          }
-        
-
+        data = {
+          pedido: res.orders[i].id,
+          fecha: res.orders[i].created,
+          nombre: res.orders[i].user.full_name,
+          estado: res.orders[i].status.name,
+          observaciones: res.orders[i].order_products[0].product.description,
+          direccion: res.orders[i].location
+            ? res.orders[i].location.address
+            : ""
+        };
         this.objects.push(data);
       }
-
-      console.log("sharannn",this.objects);
-
+      console.log("sharannn", this.objects);
     });
   }
-
 }
